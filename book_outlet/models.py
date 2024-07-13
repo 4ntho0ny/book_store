d2993ea15974a13cef8ecd865af316345c6572f2
4ntho0ny <anthonyhildebrandchendrik@gmail.com>
2024-07-13 18:00:49 -0400

    add number of books and rating average in index.html. Override  get_absolute_url  and save methods. Finally, add slug for primary key Book class

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.core.validators import MinValueValidator as Min, MaxValueValidator as Max
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[Min(1), Max(5)])
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
+
+    def get_absolute_url(self):
+        return reverse("book_dtl", args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
